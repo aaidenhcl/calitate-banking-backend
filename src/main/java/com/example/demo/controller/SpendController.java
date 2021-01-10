27abbd15c41021f6dae5af103ac3c308d7bd3bce
Samiylo <skryshu@gmail.com>
2021-01-10 14:54:21 -0600

    add Spend controller and DAO

---
+++ src/main/java/com/example/demo/controller/SpendController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.AccountRepo;
+import com.example.demo.model.Account;
+import com.example.demo.model.Spend;
+import com.example.demo.dao.SpendRepo;
+
+@RestController
+public class SpendController {
+
+		@Autowired
+		SpendRepo repo;
+		
+		@GetMapping(path="/spends/:id/edit")
+		public Spend findSpend(@RequestBody Spend spend){
+			System.out.println("In function");
+//			repo.save(spend);
+			
+			return spend;
+		}
+}
+
