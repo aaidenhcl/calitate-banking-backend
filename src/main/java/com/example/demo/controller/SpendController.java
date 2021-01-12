33ee8746acd0977d135174b005c4117c0f79586d
Samiylo <skryshu@gmail.com>
2021-01-12 11:19:49 -0600

    add create route

--- src/main/java/com/example/demo/controller/SpendController.java
+++ src/main/java/com/example/demo/controller/SpendController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dao.AccountRepo;
@@ -22,20 +23,34 @@ public class SpendController {
 		@Autowired
 		SpendRepo repo;
 		
-		//Get mapping for editing a spend
+		//Get route
 		@GetMapping(path="/spends/{id}")
 		public Spend getSpend(@PathVariable("id") Long id){
 			
 			Optional<Spend> spendOpt = repo.findById(id);
 			
+			//Test if id is not null
 			if (spendOpt == null) {
 				return null;
 			}
 			
+			//Type cast a new spend
 			Spend spend = spendOpt.get();
 			
 			
 			return spend;
+		}
+		
+		//Post Route
+		@PostMapping(path = "/spends")
+		public void create( Spend newSpend) {
+			
+			//Testing
+			System.out.println(newSpend);
+			
+			//Save 
+			repo.save(newSpend);
+			
 		}
 }
 
