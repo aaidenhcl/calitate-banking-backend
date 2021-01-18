35b3e95d3a2af4c2121625b4f9ad97a37708477f
jmathewhcl <76819300+jmathewhcl@users.noreply.github.com>
2021-01-18 18:22:19 -0500

    Added method for loanRequests for user stories 3-5

--- src/main/java/com/example/demo/bo/LoanRequestBO.java
+++ src/main/java/com/example/demo/bo/LoanRequestBO.java
@@ -1,14 +1,70 @@
 package com.example.demo.bo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.LinkedHashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.demo.dao.LoanRequestRepo;
+import com.example.demo.model.CreditCardRequest;
+import com.example.demo.model.LoanRequest;
 
 @Component
 public class LoanRequestBO {
 
 	@Autowired
 	LoanRequestRepo repo;
+	
+	 /* Grabs list of all requests and styles it for a response
+	 */
+	public List<Map<String, Object>> styleResponse(List<LoanRequest> allRequests) {
+		System.out.println("In style response");
+		
+
+		List<Map<String, Object>> all = new ArrayList<>();
+		
+		//Fill Map
+		for (LoanRequest lr : allRequests) {
+			LinkedHashMap<String, Object> responseMap = new LinkedHashMap<>();
+			
+			//Add to object map
+			responseMap.put("Request Id: ", lr.getId());
+			responseMap.put("Status: ", lr.getStatus());
+			responseMap.put("Offered Limit: ", lr.getOfferedAmount());
+			responseMap.put("APR: ", lr.getOfferedApr());
+			responseMap.put("Requested: ", lr.getRequestedTime());
+			
+			//Add object map to list
+			all.add(responseMap);
+		}
+		
+		
+		
+		return all;
+	}
+	
+	/*
+	 * converts returned List from repo to a map
+	 * sends to controller in map format to be converted to JSON with GSON
+	 */
+	public Map<String, Integer>findAllCountRejectedAndReason(){
+		List<LoanRequest> lrList= repo.findAllByStatus("rejected");
+		
+		Map<String, Integer> lrMap = new TreeMap<>();
+		
+		for(LoanRequest lr: lrList) {
+			if(lrMap.containsKey(lr.getReason())) {
+				Integer value = lrMap.get(lr.getReason());
+				lrMap.put(lr.getReason(), ++value);
+			} else {
+				lrMap.put(lr.getReason(), 1);
+			}
+		}
+		return lrMap;
+	}
 	
 }
--- src/main/java/com/example/demo/dao/LoanRequestRepo.java
+++ src/main/java/com/example/demo/dao/LoanRequestRepo.java
@@ -1,9 +1,24 @@
 package com.example.demo.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.model.CreditCardRequest;
 import com.example.demo.model.LoanRequest;
 
 public interface LoanRequestRepo extends JpaRepository<LoanRequest, Long>{
 
+	List<LoanRequest> findByUserRegion(String region);
+	List<LoanRequest> findByUserProfession(String profession);
+	List<LoanRequest> findByUserRegionAndUserProfession(String region, String profession);
+	
+	@Query(value = "select * from loan_request;", nativeQuery = true)
+	public List<LoanRequest> getStatusList();
+	
+	@Query(value = "select * from laon_request;" , nativeQuery = true)
+	public List<LoanRequest> findAllStatus();
+	
+	public List<LoanRequest> findAllByStatus(String status);
 }
